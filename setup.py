f24c851acb333b0696e2932a83a0699a86e1df89
Aysha-sithar <asitharahts@qatar.com>
2023-12-20 16:37:26 +0000

    adjusting zatca settings

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
         install_requires = f.read().strip().split('\n')
 
-# get version from __version__ variable in axis_inspection/__init__.py
-from axis_inspection import __version__ as version
+# get version from __version__ variable in zatca2023/__init__.py
+from zatca2023 import __version__ as version
 
 setup(
         name='zatca2023',
